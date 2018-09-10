4cc83f5b7eb9b5fa06b20c51561a79302e340457
Isaiah Harris <harrisfambam@gmail.com>
2018-09-09 15:59:12 -1000

    header component basic

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const Redis = require('connect-redis')(session);
+const passport = require('passport');
+
+const PORT = process.env.PORT || 8008;
+const server = express();
+const routes = require('./db/routes');
+
+// server.use(express.static('public/'))
+server.use(bodyparser.json());
+server.use(bodyparser.urlencoded({ extended: true }));
+
+server.use(
+  session({
+    store: new Redis(),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+server.use(passport.initialize());
+server.use(passport.session());
+
+server.use('/api', routes);
+
+server.get('*', (req, res) => {
+  res.sendFile('index.html');
+});
+
+server.listen(PORT, () => {
+  console.log(`Connected to port ${PORT}\n`);
+});
