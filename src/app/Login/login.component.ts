edb281dbf394a4f8809bc5043cc22f3304c3c963
Isaiah Harris <harrisfambam@gmail.com>
2018-09-10 10:18:01 -1000

    front end login

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor() {}
+  loginFormData: {
+    username: string,
+    password: string
+  } = {
+    username: '',
+    password: ''
+  };
+  constructor(private auth: AuthService, private router: Router) {}
+
+  login() {
+    return this.auth
+      .login(this.loginFormData)
+      .then(() => {
+        console.log('user logged in');
+      })
+      .then(() => {
+        this.router.navigate(['/']);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
 import { SidebarComponent } from './Sidebar/sidebar.component';
 import {LoginComponent} from './Login/login.component';
+import { RegisterComponent } from './Register/register.component';
+
+//services
+
+import { BackendService} from '../services/backend.service'
+import { AuthService } from '../services/auth.service';
+import {SessionService} from '../services/session.service';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(
-      [
-        { path: 'login', component: LoginComponent }
-      ]
-    )
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ])
   ],
-  providers: [],
+  providers: [
+    BackendService,
+    SessionService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
