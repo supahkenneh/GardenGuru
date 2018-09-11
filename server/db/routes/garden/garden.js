227a96a579fc4a7d47bdad13a7a9f01ef9c3e8fe
Mckarl Manuel <manmckarl@gmail.com>
2018-09-10 15:29:35 -1000

    GET /garden

--- server/db/routes/garden/garden.js
+++ server/db/routes/garden/garden.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
+const Crop = require('../../models/Crop');
 
 router.get('/', (req, res) => {
-  console.log('show garden');
-  res.json(`show user's garden`)
+  const id = req.user.id;
+  return Crop
+    .where({ owner_id: id })
+    .fetchAll({ withRelated: 'photo' })
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      console.log('error :', err);
+    });
 });
 
 module.exports = router;
