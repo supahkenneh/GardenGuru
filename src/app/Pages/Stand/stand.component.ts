c45529905b6f8314a2708b944176c506dbaba30f
Isaiah Harris <harrisfambam@gmail.com>
2018-09-12 16:28:35 -1000

    change to your stand has no name

--- src/app/Pages/Stand/stand.component.ts
+++ src/app/Pages/Stand/stand.component.ts
@@ -11,7 +11,7 @@ export class StandComponent implements OnInit {
   crops;
   user;
   noStand;
-  isEdit: boolean = true;
+  isEdit: boolean = false;
   garden
   editFormData = {
     stand_name: ''
