cd2a01f86fe31e70dbb049ee08bc7fbf6a9c25d7
Mckarl Manuel <manmckarl@gmail.com>
2018-09-13 14:20:56 -1000

    search-sidebar pops out, nothing in search-sidebar yet

--- src/app/Components/Search/search.component.ts
+++ src/app/Components/Search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { SessionService } from '../../Services/session.service';
+import { AuthService } from '../../Services/auth.service';
 
 @Component({
   selector: 'app-search',
@@ -8,6 +10,32 @@ import { Component } from '@angular/core';
 
 export class SearchComponent {
   user: object;
+  category: any;
 
-  
+  categories: string[] = ['Marketplace', 'Stand', 'Garden'];
+
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if (
+      event.target !== document.getElementById('search-sidebar') &&
+      event.target.id !== 'search' &&
+      document.getElementById('search-sidebar').className === 'active'
+    ) {
+      document.getElementById('search-sidebar').classList.toggle('active');
+    }
+  }
+
+  constructor(private session: SessionService, private auth: AuthService) {
+    this.user = session.getSession();
+  }
+
+  isLoggedIn() {
+    return this.session.isLoggedIn();
+  }
+
+  toggleSearchSideBar() {
+    document.getElementById('search-sidebar').classList.toggle('active');
+  }
+
+  // Search methods goes below here
 }
