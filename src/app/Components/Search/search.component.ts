2e091331b57a619e731113272af5b1c0ef07b469
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-09-20 18:24:15 -1000

    stand unauth fix, marginbottom on register form

--- src/app/Components/Search/search.component.ts
+++ src/app/Components/Search/search.component.ts
@@ -15,8 +15,6 @@ export class SearchComponent {
   category: any;
   searchInput: string = '';
 
-  categories: string[] = ['Marketplace', 'My Stand', 'My Garden'];
-
   cropSearchData: {
     category: string;
     searchInput: string;
